61578845140d25b54a610a19f38003bbd8736c88
Andrew Butenko <sedkol88@gmail.com>
2023-11-14 22:02:01 +0200

    task3 is done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      const result = { position, delay };
+
+      if (shouldResolve) {
+        resolve(result);
+      } else {
+        reject(result);
+      }
+    }, delay);
+  });
 }
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delayInput = document.querySelector('[name="delay"]');
+  const stepInput = document.querySelector('[name="step"]');
+  const amountInput = document.querySelector('[name="amount"]');
+
+  const initialDelay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  for (let i = 0; i < amount; i+=1) {
+    const position = i + 1;
+    const currentDelay = initialDelay + i * step;
+
+    createPromise(position, currentDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
